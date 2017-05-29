ac8a0ff9c0b98b58a852d4cbf8973dd91fa6c084
yujeong <yji0806@naver.com>
2017-05-29 17:28:27 +0900

    리스트에 항목추가(5/29)

--- app/src/main/java/org/androidtown/java_app/MainActivity.java
+++ app/src/main/java/org/androidtown/java_app/MainActivity.java
@@ -6,22 +6,34 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText Edthw,Edtdate;
     View Add;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("과제알리미");
+
+        final ArrayList<String> items=new ArrayList<String>();
+        final ArrayAdapter adapter=new ArrayAdapter(this,android.R.layout.simple_list_item_single_choice,items);//ListView 아이템에 TextView와 Radio Button을 가진 View 표시
+        final ListView listview=(ListView) findViewById(R.id.listview1);
+        listview.setAdapter(adapter);
+
         Button button2=(Button)findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener(){
+        button2.setOnClickListener(new View.OnClickListener(){ //미리 코딩해놓은 리스너와 연결
 
             @Override
             public void onClick(View v){
@@ -34,7 +46,10 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(DialogInterface dialog, int which){
                         Edthw=(EditText) Add.findViewById(R.id.hw);
                         Edtdate=(EditText) Add.findViewById(R.id.date);
-
+                        String str="[ "+Edtdate.getText().toString()+" ] "+Edthw.getText().toString();
+                        items.add(str);
+                        adapter.notifyDataSetChanged();
+                        Edthw.setText(""); //입력된값지우기
                     }
                 });
 
@@ -49,3 +64,4 @@ public class MainActivity extends AppCompatActivity {
 
 
 }
+
